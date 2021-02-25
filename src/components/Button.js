fe3abcb719e47f465f9927071de11f90484b8c70
Beata <beata.czekaj.7@gmail.com>
2021-02-25 20:09:04 +0100

    Added ColorContext to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,23 @@
 import React from "react";
+import ColorContext from "../contexts/ColorContext";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  render() {
+  renderButton(color) {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
           {(value) => (value === "english" ? "Submit" : "Voorleggen")}
         </LanguageContext.Consumer>
       </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   }
 }
