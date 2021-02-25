b7f40e48e582a785bf5416d669474b14878a5288
Beata <beata.czekaj.7@gmail.com>
2021-02-25 19:40:14 +0100

    added LanguageContext Provider

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LanguageContext from "../context/LanguageContext";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
   static contextType = LanguageContext;
